44cd75e52d9f3b47a92bc17217feb72e5c5bd321
Sophia <24-03168@g.batstate-u.edu.ph>
2025-12-06 23:34:02 +0800

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// PROJECT-LEVEL build.gradle.kts (root build script)
-// Do NOT place module dependencies here.
-
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-}
